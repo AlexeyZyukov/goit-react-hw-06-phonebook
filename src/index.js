3f0a943372c4fdad3e2a4ca12cf3a3b7e3a375ab
Alexey Zyukov <aalex64@ukr.net>
2022-01-17 22:43:35 +0200

    update actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
 
 import './index.css';
 
